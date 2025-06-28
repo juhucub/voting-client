67caffff72ccb3a076d3188f4a9677d178b8d60c
Jacob Gardner <juhucub@gmail.com>
2025-06-27 18:53:52 -0700

    react and react-hot-looader </3

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
+//const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+var webpack = require('webpack');
+
 module.exports = {
     mode: 'development',
     entry: [
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
         './src/index.js'
     ],
     module: {
         loaders: [{
             test: /\.jsx?$/,
             exclude: /node_modules/,
-            loader: "babel"
+            loader: "react-hot!babel"
         }]
     },
     resolve: {  
@@ -22,5 +27,11 @@ module.exports = {
         static: {
         directory: './dist',
         },
+        hot: true,
+        port: 8080,
     },
+    plugins: [
+        //new ReactRefreshWebpackPlugin()
+        new webpack.HotModuleReplacementPlugin()
+    ],
 };
